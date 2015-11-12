2d1911937ec885007a1f2eafcc214306cc11f3a6
mnemotiv <hostmaster@nodetemple.com>
2015-11-12 16:21:22 +0200

    Improve k8s master

--- install.sh
+++ install.sh
@@ -20,19 +20,26 @@ gcloud compute firewall-rules create ${CLUSTER_NAME}-ssh \
 
 echo -e "- Setting up ${ETCD_NODES_AMOUNT} etcd nodes"
 
-export ETCD_DISCOVERY_TOKEN=$(curl -s https://discovery.etcd.io/new?size=${ETCD_NODES_AMOUNT})
-source ./func.sh
-ETCD_META=$(metatmp etcd.yaml ${CLUSTER_NAME}-etcd.yaml)
-
 gcloud compute firewall-rules create ${CLUSTER_NAME}-etcd \
   --project ${PROJECT} \
   --network ${NETWORK} \
   --source-tags "${CLUSTER_NAME}-etcd" \
   --target-tags "${CLUSTER_NAME}-etcd" \
-  --allow tcp:2379-2380
+  --allow tcp:2380
+
+gcloud compute firewall-rules create ${CLUSTER_NAME}-etcd-k8s \
+  --project ${PROJECT} \
+  --network ${NETWORK} \
+  --source-tags "${CLUSTER_NAME}-etcd,${CLUSTER_NAME}-k8s-master" \
+  --target-tags "${CLUSTER_NAME}-etcd" \
+  --allow tcp:2379
+
+export ETCD_DISCOVERY_TOKEN=$(curl -s https://discovery.etcd.io/new?size=${ETCD_NODES_AMOUNT})
+source ./func.sh
+ETCD_META=$(metatmp etcd.yaml ${CLUSTER_NAME}-etcd.yaml)
 
 gcloud compute instances create $(for ETCD_INDEX in $(seq 1 ${ETCD_NODES_AMOUNT}); do echo "${CLUSTER_NAME}-etcd-${ETCD_INDEX}"; done) \
-  --tags "${CLUSTER_NAME}-etcd,${CLUSTER_NAME}" \
+  --tags "${CLUSTER_NAME},${CLUSTER_NAME}-etcd" \
   --project ${PROJECT} \
   --network ${NETWORK} \
   --zone ${ZONE} \
@@ -44,5 +51,24 @@ gcloud compute instances create $(for ETCD_INDEX in $(seq 1 ${ETCD_NODES_AMOUNT}
   --can-ip-forward \
   --no-scopes \
   --metadata-from-file user-data=${ETCD_META}
+
+echo -e "- Setting up k8s master node"
+
+source ./func.sh
+K8S_MASTER_META=$(metatmp k8s-master.yaml ${CLUSTER_NAME}-k8s-master.yaml)
+
+gcloud compute instances create ${CLUSTER_NAME}-k8s-master \
+  --tags "${CLUSTER_NAME},${CLUSTER_NAME}-k8s-master" \
+  --project ${PROJECT} \
+  --network ${NETWORK} \
+  --zone ${ZONE} \
+  --machine-type n1-standard-1 \
+  --image-project coreos-cloud \
+  --image coreos-alpha-845-0-0-v20151025 \
+  --boot-disk-type pd-ssd \
+  --boot-disk-size 30GB \
+  --can-ip-forward \
+  --no-scopes \
+  --metadata-from-file user-data=${K8S_MASTER_META}
 
 echo -e "- All tasks completed"
