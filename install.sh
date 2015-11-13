442f3830a00780cd77b2191a3e2fb8f3b4d6c564
mnemotiv <hostmaster@nodetemple.com>
2015-11-13 13:12:43 +0200

    Fix instances string

--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@ gcloud compute instances create $(for ETCD_INDEX in $(seq 1 ${ETCD_NODES_AMOUNT}
   --no-scopes \
   --metadata-from-file user-data=${ETCD_META}
 
-ETCD_INSTANCES=$(for ETCD_INDEX in $(seq 1 ${ETCD_NODES_AMOUNT}); do if [ ${ETCD_INDEX} -gt 1 ]; then echo -e "\b,"; fi; echo -e "\b${CLUSTER_NAME}-etcd-${ETCD_INDEX}"; done)
+ETCD_INSTANCES=$(echo $(for ETCD_INDEX in $(seq 1 ${ETCD_NODES_AMOUNT}); do if [ ${ETCD_INDEX} -gt 1 ]; then echo ","; fi; echo "${CLUSTER_NAME}-etcd-${ETCD_INDEX}"; done) | sed "s/\s//g")
 
 gcloud compute firewall-rules create ${CLUSTER_NAME}-etcd-internal \
   --network ${NETWORK} \
