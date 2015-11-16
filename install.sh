65e658fda20e8aca12c25a3bc8e7452c446c9798
mnemotiv <hostmaster@nodetemple.com>
2015-11-16 15:53:06 +0200

    Refactor network and firewall

--- install.sh
+++ install.sh
@@ -11,11 +11,37 @@ gcloud config set compute/zone ${ZONE}
 
 echo -e "- Setting up initial firewall rules"
 
-gcloud compute firewall-rules create ${CLUSTER_NAME}-ssh \
-  --network ${NETWORK} \
-  --source-ranges "0.0.0.0/0" \
-  --target-tags "${CLUSTER_NAME}" \
-  --allow tcp:22
+gcloud compute networks create ${CLUSTER_NAME}-network \
+  --range ${NETWORK_RANGE}
+
+gcloud compute firewall-rules create ${CLUSTER_NAME}-allow-external-ssh \
+  --network ${CLUSTER_NAME}-network \
+  --allow tcp:22 \
+  --source-ranges 0.0.0.0/0
+
+gcloud compute firewall-rules create ${CLUSTER_NAME}-allow-internal-etcd-peers \
+  --network ${CLUSTER_NAME}-network \
+  --allow tcp:2380 \
+  --source-tags ${CLUSTER_NAME}-master \
+  --target-tags ${CLUSTER_NAME}-master
+
+gcloud compute firewall-rules create ${CLUSTER_NAME}-allow-internal-etcd-clients \
+  --network ${CLUSTER_NAME}-network \
+  --allow tcp:2379,icmp \
+  --source-ranges ${NETWORK_RANGE} \
+  --target-tags ${CLUSTER_NAME}-master
+
+gcloud compute firewall-rules create ${CLUSTER_NAME}-allow-internal-flannel-vxlan \
+  --network ${CLUSTER_NAME}-network \
+  --allow udp:8472 \
+  --source-ranges ${NETWORK_RANGE} \
+  --target-tags ${CLUSTER_NAME}
+
+gcloud compute firewall-rules create ${CLUSTER_NAME}-allow-internal-k8s-api \
+  --network ${CLUSTER_NAME}-network \
+  --allow tcp:443 \
+  --source-ranges ${NETWORK_RANGE} \
+  --target-tags ${CLUSTER_NAME}-master
 
 echo -e "- Setting up ${MASTER_NODES_AMOUNT} master nodes"
 
@@ -24,7 +50,7 @@ source ./func.sh
 NODE_META=$(metatmp k8s-master.yaml ${CLUSTER_NAME}-k8s-master.yaml)
 
 gcloud compute instances create $(for NODES_INDEX in $(seq 1 ${MASTER_NODES_AMOUNT}); do echo "${CLUSTER_NAME}-master-${NODES_INDEX}"; done) \
-  --tags "${CLUSTER_NAME},${CLUSTER_NAME}-master" \
+  --tags ${CLUSTER_NAME},${CLUSTER_NAME}-master \
   --zone ${ZONE} \
   --network ${NETWORK} \
   --machine-type n1-standard-1 \
@@ -33,10 +59,9 @@ gcloud compute instances create $(for NODES_INDEX in $(seq 1 ${MASTER_NODES_AMOU
   --boot-disk-type pd-ssd \
   --boot-disk-size 30GB \
   --can-ip-forward \
-  --scopes compute-rw \
+  --no-scopes \
   --metadata-from-file user-data=${NODE_META}
 
 #wget https://storage.googleapis.com/kubernetes-release/release/v1.1.1/bin/linux/amd64/kubelet
-#https://github.com/kelseyhightower/coreos-ops-tutorial/blob/master/kube-kubelet.service
 
 echo -e "- All tasks completed"
