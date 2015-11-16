21cd7f51cedbdf43b14cd7f973bd62a3eea3411d
mnemotiv <hostmaster@nodetemple.com>
2015-11-16 16:03:20 +0200

    GCE creates internet route gateway automatically

--- install.sh
+++ install.sh
@@ -14,12 +14,6 @@ echo -e "- Setting up internal network"
 gcloud compute networks create ${CLUSTER_NAME}-network \
   --range ${NETWORK_RANGE}
 
-gcloud compute routes create ${CLUSTER_NAME}-internet \
-  --network ${CLUSTER_NAME}-network \
-  --destination-range 0.0.0.0/0 \
-  --next-hop-gateway default-internet-gateway \
-  --priority 1000
-
 echo -e "- Setting up firewall rules"
 
 gcloud compute firewall-rules create ${CLUSTER_NAME}-allow-external-ssh \
