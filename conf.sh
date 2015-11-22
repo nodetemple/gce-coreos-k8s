c9b11fcbed708278c313c730473ec4548bf46e7f
mnemotiv <hostmaster@nodetemple.com>
2015-11-22 15:41:40 +0200

    Setup initial routes for etcd

--- conf.sh
+++ conf.sh
@@ -16,13 +16,13 @@ export ZONE=${REGION}-d
 export MASTER_NODES_AMOUNT=3
 
 # The CIDR network to use for service cluster VIPs (Virtual IPs). Each service will be assigned a cluster IP out of this range. This must not overlap with any IP ranges assigned to the POD_NETWORK, or other existing network infrastructure. Routing to these VIPs is handled by a local kube-proxy service to each node, and are not required to be routable between nodes.
-export SERVICE_IP_RANGE=10.2.0.0/24
+export SERVICE_IP_RANGE=10.3.0.0/24
 
 # The VIP (Virtual IP) address of the Kubernetes API Service. If the SERVICE_IP_RANGE is changed above, this must be set to the first IP in that range.
-export K8S_SERVICE_IP=10.2.0.1
+export K8S_SERVICE_IP=10.3.0.1
 
 # The VIP (Virtual IP) address of the cluster DNS service. This IP must be in the range of the SERVICE_IP_RANGE and cannot be the first IP in the range. This same IP must be configured on all worker nodes to enable DNS service discovery.
-export DNS_SERVICE_IP=10.2.0.10
+export DNS_SERVICE_IP=10.3.0.10
 
 # The CIDR network to use for pod IPs. Each pod launched in the cluster will be assigned an IP out of this range. This network must be routable between all nodes in the cluster. In a default installation, the flannel overlay network will provide routing to this network.
-export POD_NETWORK=10.3.0.0/16
+export POD_NETWORK=10.4.0.0/16
--- install.sh
+++ install.sh
@@ -16,40 +16,51 @@ gcloud compute networks create ${CLUSTER_NAME}-network \
 
 echo -e "- Setting up firewall rules"
 
-gcloud compute firewall-rules create ${CLUSTER_NAME}-allow-external-ssh \
+gcloud compute firewall-rules create ${CLUSTER_NAME}-external-ssh \
   --network ${CLUSTER_NAME}-network \
   --allow tcp:22 \
   --source-ranges 0.0.0.0/0
 
-gcloud compute firewall-rules create ${CLUSTER_NAME}-allow-internal-icmp \
+gcloud compute firewall-rules create ${CLUSTER_NAME}-internal-icmp \
   --network ${CLUSTER_NAME}-network \
   --allow icmp \
   --source-ranges ${NETWORK_RANGE} \
   --target-tags ${CLUSTER_NAME}
 
-gcloud compute firewall-rules create ${CLUSTER_NAME}-allow-internal-etcd-peers \
+gcloud compute firewall-rules create ${CLUSTER_NAME}-internal-etcd-peers \
   --network ${CLUSTER_NAME}-network \
   --allow tcp:2380 \
   --source-tags ${CLUSTER_NAME}-master \
   --target-tags ${CLUSTER_NAME}-master
 
-gcloud compute firewall-rules create ${CLUSTER_NAME}-allow-internal-etcd-clients \
+gcloud compute firewall-rules create ${CLUSTER_NAME}-internal-etcd-clients \
   --network ${CLUSTER_NAME}-network \
   --allow tcp:2379 \
   --source-ranges ${NETWORK_RANGE} \
   --target-tags ${CLUSTER_NAME}-master
 
-gcloud compute firewall-rules create ${CLUSTER_NAME}-allow-internal-flannel-vxlan \
+gcloud compute firewall-rules create ${CLUSTER_NAME}-internal-flannel-vxlan \
   --network ${CLUSTER_NAME}-network \
   --allow udp:8472 \
   --source-ranges ${NETWORK_RANGE} \
   --target-tags ${CLUSTER_NAME}
 
-gcloud compute firewall-rules create ${CLUSTER_NAME}-allow-internal-k8s-api \
+gcloud compute firewall-rules create ${CLUSTER_NAME}-internal-k8s-api \
   --network ${CLUSTER_NAME}-network \
   --allow tcp:443 \
   --source-ranges ${NETWORK_RANGE} \
   --target-tags ${CLUSTER_NAME}-master
+
+echo -e "- Setting up routes"
+
+for NODES_INDEX in $(seq 1 ${MASTER_NODES_AMOUNT})
+do
+  gcloud compute routes create ${CLUSTER_NAME}-master-${NODES_INDEX} \
+    --network ${CLUSTER_NAME}-network \
+    --priority 100 \
+    --destination-range 10.2.0.${NODES_INDEX} \
+    --next-hop-instance ${CLUSTER_NAME}-master-${NODES_INDEX}
+done
 
 echo -e "- Setting up ${MASTER_NODES_AMOUNT} master nodes"
 
